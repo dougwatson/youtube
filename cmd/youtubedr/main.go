0d6ce97118020655d6e6d8e2db29391c4832e154
Doug Watson <dougwatson@gocoder.io>
2022-12-29 08:04:25 -0600

    updating fork with replace in go.mod

--- cmd/youtubedr/main.go
+++ cmd/youtubedr/main.go
@@ -5,8 +5,20 @@ import (
 	"os"
 )
 
+//var testDownloader = func() (dl Downloader) {
+//	dl.OutputDir = "download_test"
+//	dl.Debug = true
+//	return
+//}()
+
 func main() {
 	exitOnError(rootCmd.Execute())
+	//ctx := context.Background()
+
+	//video, err := testDownloader.Client.GetVideoContext(ctx, "BaW_jenozKc")
+	//require.NoError(err)
+
+	//require.NoError(testDownloader.DownloadComposite(ctx, "", video, "hd1080", "mp4"))
 }
 
 func exitOnError(err error) {
